4d97ca95ca8dbd67c4b154f27c8495cf32c2aee8
Mike Fisher <mikefisher84@gmail.com>
2025-03-24 19:13:23 -0400

    Refactor nvim configs into separate apps.

---
+++ dot_config/nvim-lazy/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+vim.g.snacks_animate = false
+
+-- Add any additional options here
